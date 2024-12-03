da1f16786ac54489e86ad8547dd3a9de19669b87
Youssef <yousef.ahmed.hani@gmail.com>
2024-12-03 20:12:41 +0200

    did assignment 2

---
+++ wekk-2/q1.js
@@ -0,0 +1,14 @@
+let num1 = 10;
+let num2 = 20;
+
+console.log(num1 + "" + num2);
+//i don't know if thats what he wants. if he wants the output to be a number then i can change it to typeof (num1+num2)
+console.log(typeof (num1 + "" + num2));
+
+console.log(`${num1}${num2}`);
+// same here
+console.log(typeof `${num1}${num2}`);
+
+console.log(num1 + "\n" + num2);
+console.log(`${num1}
+${num2}`);
