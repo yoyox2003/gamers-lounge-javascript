9bbb6ccc84f9b2af4eda82397bd8bbe6ed043f4d
Youssef <yousef.ahmed.hani@gmail.com>
2024-12-04 22:37:09 +0200

    finished task 2 of week-4

---
+++ week-4/task-2/js/q1.js
@@ -0,0 +1,7 @@
+let userName = "Elzero";
+console.log(userName[0]); // e
+console.log(userName.charAt(0)); // e
+console.log(userName.slice(0, 1)); // e
+console.log(userName.substring(0, 1)); // e
+console.log(userName.substr(0, 1)); // e
+console.log(userName.slice(0, 1).repeat(3)); // eee
---
+++ week-4/task-2/js/q2.js
@@ -0,0 +1,10 @@
+console.log("");
+
+let word = "Elzero";
+let letterZ = "z";
+let letterE = "e";
+let letterO = "O";
+
+console.log(userName.startsWith("E")); // True
+console.log(userName.endsWith("o")); // True
+console.log(userName.includes("z")); // True
