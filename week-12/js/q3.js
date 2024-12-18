3aa3221098fae5498ddd68d9e50ec811254b3f9f
Youssef <yousef.ahmed.hani@gmail.com>
2024-12-18 22:34:07 +0200

    changed to onInput

--- week-12/js/q3.js
+++ week-12/js/q3.js
@@ -3,7 +3,7 @@ let result = document.getElementsByClassName("result")[0];
 let text = document.createTextNode("{0} USD Dollar = {0} Egyptian Pound");
 
 let currencyValue = currency.value;
-currency.onclick = function () {
+currency.oninput = function () {
     currencyValue = currency.value;
     result.textContent = `{${currencyValue}} USD Dollar = {${(
         currencyValue * 49.3
